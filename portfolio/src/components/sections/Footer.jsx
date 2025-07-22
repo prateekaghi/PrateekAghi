af4752640f34b24e2243ee9a7708417d3a1a243a
Prateek Aghi <prateek@samanalytic.com>
2025-07-22 08:14:17 +0530

    footer fix

--- portfolio/src/components/sections/Footer.jsx
+++ portfolio/src/components/sections/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer({ scrollTo }) {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Code className="h-6 w-6" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built with Next.js and Tailwind CSS. © 2024 Alex Johnson.
+            Built with Next.js and Tailwind CSS.
           </p>
         </div>
       </div>
